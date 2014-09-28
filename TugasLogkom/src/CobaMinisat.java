ada08b73de39e2fbc642430949ba77acd893b48a
irheza <irheza@live.com>
2014-09-28 23:51:08 +0700

    Minisat + librarynya harusnya (semoga ke upload)

---
+++ TugasLogkom/src/CobaMinisat.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.reader.DimacsReader;
+import org.sat4j.reader.ParseFormatException;
+import org.sat4j.reader.Reader;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
+
+
+/**
+ *
+ * @author Rhesa
+ */
+public class CobaMinisat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws TimeoutException, ContradictionException {
+                final int MAXVAR = 1000000;
+        final int NBCLAUSES = 3;
+
+        ISolver solver = SolverFactory.newDefault();
+
+        // prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
+        solver.newVar(MAXVAR);
+        
+        solver.setExpectedNumberOfClauses(NBCLAUSES);
+        // Feed the solver using Dimacs format, using arrays of int
+        // (best option to avoid dependencies on SAT4J IVecInt)
+        for (int i=0; i<NBCLAUSES;i++) {
+          int [] clause ={1,-3,7}; // get the clause from somewhere
+          // the clause should not contain a 0, only integer (positive or negative)
+          // with absolute values less or equal to MAXVAR
+          // e.g. int [] clause = {1, -3, 7}; is fine
+          // while int [] clause = {1, -3, 7, 0}; is not fine 
+          solver.addClause(new VecInt(clause)); // adapt Array to IVecInt
+        }
+
+        // we are done. Working now on the IProblem interface
+        IProblem problem = solver;
+         Reader reader = new DimacsReader(solver);
+        if (problem.isSatisfiable()) {
+            System.out.println("ho dapet");
+              System.out.println(reader.decode(problem.model()));
+        } else {
+            System.out.println("gagal");
+        }
+    }
+    
+    
+}
