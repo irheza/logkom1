88be806e3a70586b6eab08ebeb0655178a6af86d
Victoria Lestari <lovesmiles07@gmail.com>
2014-09-29 22:25:28 +0700

    commit baru

--- TugasLogkom/src/CobaMinisat.java
+++ TugasLogkom/src/CobaMinisat.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.sat4j.core.VecInt;
@@ -17,7 +16,6 @@ import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.TimeoutException;
 
-
 /**
  *
  * @author Rhesa
@@ -28,36 +26,35 @@ public class CobaMinisat {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws TimeoutException, ContradictionException {
-                final int MAXVAR = 1000000;
+        final int MAXVAR = 1000000;
         final int NBCLAUSES = 3;
 
         ISolver solver = SolverFactory.newDefault();
 
         // prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
         solver.newVar(MAXVAR);
-        
+
         solver.setExpectedNumberOfClauses(NBCLAUSES);
         // Feed the solver using Dimacs format, using arrays of int
         // (best option to avoid dependencies on SAT4J IVecInt)
-        for (int i=0; i<NBCLAUSES;i++) {
-          int [] clause ={1,-3,7}; // get the clause from somewhere
-          // the clause should not contain a 0, only integer (positive or negative)
-          // with absolute values less or equal to MAXVAR
-          // e.g. int [] clause = {1, -3, 7}; is fine
-          // while int [] clause = {1, -3, 7, 0}; is not fine 
-          solver.addClause(new VecInt(clause)); // adapt Array to IVecInt
+        for (int i = 0; i < NBCLAUSES; i++) {
+            int[] clause = {1, -3, 7}; // get the clause from somewhere
+            // the clause should not contain a 0, only integer (positive or negative)
+            // with absolute values less or equal to MAXVAR
+            // e.g. int [] clause = {1, -3, 7}; is fine
+            // while int [] clause = {1, -3, 7, 0}; is not fine 
+            solver.addClause(new VecInt(clause)); // adapt Array to IVecInt
         }
 
         // we are done. Working now on the IProblem interface
         IProblem problem = solver;
-         Reader reader = new DimacsReader(solver);
+        Reader reader = new DimacsReader(solver);
         if (problem.isSatisfiable()) {
             System.out.println("ho dapet");
-              System.out.println(reader.decode(problem.model()));
+            System.out.println(reader.decode(problem.model()));
         } else {
             System.out.println("gagal");
         }
     }
-    
-    
+
 }
--- TugasLogkom/src/gui/MapViewer.java
+++ TugasLogkom/src/gui/MapViewer.java
@@ -3,23 +3,118 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author V Anugrah
  */
-public class MapViewer{
+public class MapViewer {
+
     JFrame thisFrame;
-    public MapViewer(){
+    JPanel mainPanel; // panel utama untuk menampung semua
+    Rectangle r1;
+
+    public MapViewer() {
+        createPanel();
         thisFrame = new JFrame();
+        thisFrame.add(mainPanel);
         thisFrame.setSize(400, 400);
         thisFrame.setTitle("Tugas Logkom");
         thisFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        thisFrame.setVisible(true);
-        thisFrame.setLocationRelativeTo(null);        
-    }    
+        thisFrame.setLocationRelativeTo(null);
+    }
+
+    public void createPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setBackground(Color.white);        
+        final MyRectangle mr = new MyRectangle(50,50,Color.RED); 
+        final MyMouseListener ml = new MyMouseListener();
+        mainPanel.addMouseListener(ml);
+        
+//        mr.addMouseListener(ml);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(new JLabel("halo"), BorderLayout.NORTH);
+        mainPanel.add(mr, BorderLayout.CENTER);
+        
+        JButton freeze = new JButton("freeze");
+        mainPanel.add(freeze, BorderLayout.SOUTH);
+        freeze.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                mr.removeMouseListener(ml);
+            }
+        });
+    }
+    private class MyMouseListener implements MouseListener{
+        
+        @Override
+        public void mouseClicked(MouseEvent me) {
+            System.out.println("Click me " + me.getX() + " " + me.getY());
+        }
+
+        @Override
+        public void mousePressed(MouseEvent me) {
+            System.out.println("Press me");
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent me) {
+            System.out.println("Release me");
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent me) {
+            System.out.println("Enter me");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent me) {
+            System.out.println("Exit me");
+        }
+    }
+    
+    
+    private class MyRectangle extends JComponent{
+        Rectangle rect1;
+        Rectangle rect2;
+        int x, y;
+        Color c;
+        
+        public MyRectangle(int x, int y, Color c){
+            this.x = x;
+            this.y = y;
+            this.c = c;
+        }
+        
+        @Override
+        public void paintComponent(Graphics g){
+            Graphics2D g2 = (Graphics2D) g;
+            rect1 = new Rectangle(x, y, 100, 100);
+                        
+            g2.setColor(c);
+            g2.fill(rect1);
+            
+            rect2 = new Rectangle(100,100, 200, 50);
+            g2.setColor(Color.YELLOW);
+            g2.fill(rect2);
+        }
+    }
 }
